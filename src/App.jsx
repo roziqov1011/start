33d68fc0ca994519bc3e08b350d21fb7e0415815
roziqov1011 <roziqov1011@gmail.com>
2024-05-28 17:13:19 +0500

    inial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Portfolio from './pages/Portfolio/Portfolio'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='App'>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/portfolio' element={<Portfolio/>}/>
+        <Route path='/services' element={<h1>Service</h1>}/>
+        <Route path='/contact' element={<h1>Contact</h1>}/>
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Header() {
+    return (
+        <div className='header'>
+            <nav className="navbar navbar-expand-lg ">
+                <div className="container d-flex justify-content-between">
+                    <Link className="navbar-brand" href="#">Logo</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link to='/' className="nav-link active" aria-current="page" href="#">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to='/portfolio' className="nav-link" href="#">Portfolio</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to='/services' className="nav-link" href="#">services</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to='/contact' className="nav-link" href="#">contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
